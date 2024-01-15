53cd28c04e44931d515ab500ec197d5709b26952
Martin Lidgren <martin.lidgren@live.se>
2024-01-15 18:03:54 +0100

    Styling for My Ratings

--- src/main/java/com/example/cinescore/controller/MyRatingsViewController.java
+++ src/main/java/com/example/cinescore/controller/MyRatingsViewController.java
@@ -1,11 +1,21 @@
 package com.example.cinescore.controller;
 
+import com.example.cinescore.model.UserManager;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableView;
 
-public class MyRatingsViewController {
-    public TableView showsTable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyRatingsViewController implements Initializable {
+    public TableView<String> showsTable;
     public Tab showsTab;
     public TableView moviesTable;
     public Tab moviesTab;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
